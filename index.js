d6834c9d3a4509a418ffbfa860b01ae2647f6894
arronrees <64908445+arronrees@users.noreply.github.com>
2021-10-10 15:04:09 +0100

    Initial project structure

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// mongodb connection
+const PORT = process.env.PORT;
+const DB_URI = process.env.DB_URI;
+
+// once connected to mongoose start listening on server
+mongoose
+  .connect(DB_URI)
+  .then((res) =>
+    app.listen(PORT, () => {
+      console.log(`Mongo connected and server running on port ${PORT}`);
+    })
+  )
+  .catch((err) => console.log(err));
