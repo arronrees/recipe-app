2f47ea110cf5856011116b7e82d1b30e494250fb
arronrees <64908445+arronrees@users.noreply.github.com>
2021-10-14 19:41:26 +0100

    Update user signup handler to include jwt and not send password back to user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,13 @@
+require('dotenv').config();
+
 const User = require('../models/User');
 
 const { joiUserSchema } = require('../models/joiSchemas/joiUser');
-
 const { handleUserErrors } = require('../utils/handleErrors');
+const { createToken } = require('../utils/createToken');
+
+// 1 day in seconds
+const maxAge = 24 * 60 * 60;
 
 module.exports.validateUser = (req, res, next) => {
   const { body } = req;
@@ -23,11 +28,18 @@ module.exports.validateUser = (req, res, next) => {
 
 module.exports.postNewSignUp = async (req, res) => {
   const { body } = req;
+  const { email, password } = body;
+
+  console.log(email, password);
 
   try {
     const newUser = await User.create(body);
+    const token = createToken(newUser._id);
 
-    res.json(newUser);
+    const userToShow = { ...newUser._doc, password: 'hidden' };
+
+    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.status(200).json(userToShow);
   } catch (err) {
     const errors = handleUserErrors(err);
 
